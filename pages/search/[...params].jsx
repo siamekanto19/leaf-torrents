a6d4e5dda6151958bc1bfa9f4caa8a8a0f36873a
Siam <siamekanto360@gmail.com>
2021-04-29 06:41:54 +0600

    Update [...params].jsx

--- pages/search/[...params].jsx
+++ pages/search/[...params].jsx
@@ -4,11 +4,11 @@ import { useRouter } from 'next/router'
 import Torrent from '../../components/Torrent'
 
 export const getServerSideProps = async (context) => {
-  // Get the Queries from URL
+  // Get the Query from URL
   const keyword = context.query.params[0]
   const page = context.query.page
   // Make the API Request to fetch torrents
-  const host = `https://leaftorrents-7sfgw.ondigitalocean.app/`
+  const host = `https://leaftorrents-7sfgw.ondigitalocean.app`
   const url = `${host}/api/search/${keyword}/${page}`
   const res = await fetch(url).then((res) => res.json())
   // Redirect to 404 page if no torrent is found
